5e52e4c575a25a4e2771357986eb2228aac4fa0e
Joe Zhou <zhoushuqun@gmail.com>
2014-11-03 21:20:42 +0800

    XChange changed their interface again.

--- huobi-client-rest/src/main/java/com/redv/huobi/service/polling/HUOBITradeService.java
+++ huobi-client-rest/src/main/java/com/redv/huobi/service/polling/HUOBITradeService.java
@@ -17,10 +17,10 @@ import com.xeiam.xchange.ExchangeSpecification;
 import com.xeiam.xchange.NotAvailableFromExchangeException;
 import com.xeiam.xchange.NotYetImplementedForExchangeException;
 import com.xeiam.xchange.currency.CurrencyPair;
-import com.xeiam.xchange.dto.marketdata.Trades;
 import com.xeiam.xchange.dto.trade.LimitOrder;
 import com.xeiam.xchange.dto.trade.MarketOrder;
 import com.xeiam.xchange.dto.trade.OpenOrders;
+import com.xeiam.xchange.dto.trade.UserTrades;
 import com.xeiam.xchange.service.polling.PollingTradeService;
 
 public class HUOBITradeService extends HUOBITradeServiceRaw implements
@@ -105,7 +105,7 @@ public class HUOBITradeService extends HUOBITradeServiceRaw implements
 	 * {@inheritDoc}
 	 */
 	@Override
-	public Trades getTradeHistory(Object... arguments) {
+	public UserTrades getTradeHistory(Object... arguments) {
 		throw new NotAvailableFromExchangeException();
 	}
 
