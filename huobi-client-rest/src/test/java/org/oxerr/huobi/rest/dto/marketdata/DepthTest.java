cfccfa102c72b8fe1daec0b4ea1c697b89eb80f9
Joe Zhou <zhoushuqun@gmail.com>
2015-03-18 22:34:26 +0800

    HUOBI flipped the ask order in depth.

--- huobi-client-rest/src/test/java/org/oxerr/huobi/rest/dto/marketdata/DepthTest.java
+++ huobi-client-rest/src/test/java/org/oxerr/huobi/rest/dto/marketdata/DepthTest.java
@@ -6,15 +6,14 @@ import java.io.IOException;
 import java.math.BigDecimal;
 
 import org.junit.Test;
-import org.oxerr.huobi.rest.dto.marketdata.Depth;
 
 public class DepthTest extends UnmarshalTest {
 
 	@Test
 	public void test() throws IOException {
 		Depth depth = readValue(getClass(), "depth.json", Depth.class);
-		assertEquals(new BigDecimal("90.8"), depth.getAsks()[0][0]);
-		assertEquals(new BigDecimal("0.5"), depth.getAsks()[0][1]);
+		assertEquals(new BigDecimal("90.7"), depth.getAsks()[0][0]);
+		assertEquals(new BigDecimal("0.01"), depth.getAsks()[0][1]);
 		assertEquals(new BigDecimal("86.06"), depth.getBids()[0][0]);
 		assertEquals(new BigDecimal("79.243"), depth.getBids()[0][1]);
 	}
