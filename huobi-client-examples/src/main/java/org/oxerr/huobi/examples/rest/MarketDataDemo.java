1e7dc1976a34d20d56a9bcbba081938c1cb13f87
Joe Zhou <zhoushuqun@gmail.com>
2015-03-18 22:18:12 +0800

    HUOBI flipped the ask order in depth.

--- huobi-client-examples/src/main/java/org/oxerr/huobi/examples/rest/MarketDataDemo.java
+++ huobi-client-examples/src/main/java/org/oxerr/huobi/examples/rest/MarketDataDemo.java
@@ -1,6 +1,7 @@
 package org.oxerr.huobi.examples.rest;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 import org.oxerr.huobi.xchange.HuobiExchange;
 import org.slf4j.Logger;
@@ -32,6 +33,20 @@ public class MarketDataDemo {
 		// Depth
 		OrderBook depth = marketDataService.getOrderBook(CurrencyPair.BTC_CNY);
 		log.info("BTC depth: {}", depth);
+		if (depth.getBids().size() >= 2) {
+			BigDecimal bid0 = depth.getBids().get(0).getLimitPrice();
+			BigDecimal bid1 = depth.getBids().get(1).getLimitPrice();
+			if (bid0.compareTo(bid1) <= 0) {
+				throw new RuntimeException("bids in depth should be ordered from highest to lowest.");
+			}
+		}
+		if (depth.getAsks().size() >= 2) {
+			BigDecimal ask0 = depth.getAsks().get(0).getLimitPrice();
+			BigDecimal ask1 = depth.getAsks().get(1).getLimitPrice();
+			if (ask0.compareTo(ask1) >= 0) {
+				throw new RuntimeException("asks in depth should be ordered from lowest to highest.");
+			}
+		}
 
 		depth = marketDataService.getOrderBook(CurrencyPair.LTC_CNY);
 		log.info("LTC depth: {}", depth);
